4c6b5151868cc7e08b2b912c49b580f16085f94e
on3dd <onedeadwave@gmail.com>
2019-06-13 02:47:25 +1000

    Created main game logic and structure

---
+++ js/main.js
@@ -0,0 +1,236 @@
+// [TODO]: Новые типы противников, рандомный выбор
+// [TODO]: Новые типы оружия, механика с боеприпасами
+// [TOOD]: Добавить режим автоматической стрельбы по mousedown/mouseup и setInterval/clearInterval
+// [TODO]: Меню перед началом игры, возможность ставить игру на паузу
+// [TODO]: Таблица счета
+
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+
+// Преднастройки игры
+let renderTimer;
+let spanwTimer;
+let spawnTime = 2500;
+let scoreCount = 0;
+let livesCount = 5;
+let vx = 0;
+
+const fps = 1000/30;
+const center = canvas.height/2 - 30;
+
+document.querySelector('#score-counter').innerHTML = scoreCount;
+document.querySelector('#lives-counter').innerHTML = livesCount;
+
+// Массивы снарядов и врагов соответственно
+let bullets = [];
+let enemies = [];
+
+// Основной класс для всех двигающихся юнитов
+class Unit {
+  constructor(x, y, img) {
+    this.x = x || canvas.width;
+    this.y = y || center;
+    this.width = 80;
+    this.height = 60;
+    this.hp = 2;
+    this.isShielded = false;
+    this.img = img || enemy_img;
+  }
+  shield() {
+    this.isShielded = true;
+    setTimeout(() => this.isShielded = false, 500);
+  }
+  draw() {
+    if (this.isShielded)  {
+      ctx.save();
+      ctx.globalAlpha = 0.4;
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+      ctx.restore();
+    } 
+    else {
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+  }
+}
+
+// Подкласс для юнита игрока
+class Player extends Unit {
+  constructor(x, y, img) {
+    super(x, y, img);
+    this.hp = livesCount;
+  }
+  move(mouseY) {
+    this.y = mouseY;
+  }
+  shoot(mouseY) {
+    let bullet = new Gun(10, mouseY);
+    bullets.push(bullet)
+  }
+}
+
+// Подкласс для вражеских юнитов
+class EnemyPlane extends Unit {
+  constructor(x, y, speed) {
+    super(x, y);
+    this.speed = speed || 6;
+    this.value = 50;
+  }
+  move() {
+    this.x -= this.speed;
+  }
+}
+
+// Под-подкласс для недефолтных вражеских юнитов
+class NotDefaultEnemyPlane extends EnemyPlane {
+  constructor(x, y, speed) {
+    super(x, y);
+    this.speed = speed;
+  }
+}
+
+// Основной класс для всех снарядов
+class Bullet {
+  constructor(x, y, img) {
+    this.x = x;
+    this.y = y;
+    this.img = img || gun_img;
+  }
+  move() {
+    this.x += this.speed;
+  }
+  checkCollision() {
+    let findEnemy = enemies.find(enemy => ( ( (this.y + 15 - enemy.y >= 0) && (this.y + 15 - enemy.y <= 60) ) 
+    && (this.x - enemy.x >= 0) && (this.x - enemy.x <= this.speed) && (!enemy.isShielded) ) );
+    // Если было найдено совпадение - обрабатываем
+    if (findEnemy) { 
+      findEnemy.hp -= this.dmg;
+      bullets.splice(bullets.indexOf(this), 1);
+      // Если hp самолета < 1 - удаляем
+      // Иначе вешаем щит
+      if (findEnemy.hp <= 0) {
+        incScore(findEnemy.value);
+        enemies.splice(enemies.indexOf(findEnemy), 1);
+      }
+      else findEnemy.shield();
+    }
+    else {
+      ctx.drawImage(this.img, this.x, this.y, 60, 30);
+    }
+  }
+}
+
+// Подкласс для снарядов из обычной пушки
+class Gun extends Bullet {
+  constructor(x, y, img) {
+    super(x, y, img);
+    this.dmg = 1;
+    this.speed = 100;
+  }
+}
+
+// Отрисовка игрового поля
+function renderGame() {
+  renderTimer = setInterval(() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.drawImage(clouds_1, vx, 0);
+    ctx.drawImage(clouds_2, vx, 0);
+    ctx.drawImage(clouds_3, vx, 0);
+    ctx.drawImage(clouds_4, vx, 0);
+    ctx.drawImage(clouds_1, vx+1920, 0);
+    ctx.drawImage(clouds_2, vx+1920, 0);
+    ctx.drawImage(clouds_3, vx+1920, 0);
+    ctx.drawImage(clouds_4, vx+1920, 0);
+    player.draw();
+
+    // Двигаем все снаряды
+    bullets.forEach(el => {
+      el.move();
+      el.checkCollision();
+      // ctx.drawImage(gun_img, el.x, el.y, 60, 30);
+
+      // Если снаряд вылетел за границы экрана - удаляем
+      if (el.x >= canvas.width) bullets.splice(bullets.indexOf(el), 1);
+    })
+
+    // Двигаем всех вражеских юнитов
+    enemies.forEach(el => {
+      el.move();
+      el.draw();
+      // Если юнит вышел за границы экрана - удаляем
+      if (el.x <= -80) {
+        // Если игрок не под щитом - вычитаем урон
+        if (!player.isShielded) decLives(1);
+        // Если hp игрока < 1 - конец игры
+        if (livesCount <= 0) clearInterval(renderTimer);
+        player.shield();
+        enemies.splice(enemies.indexOf(el), 1);
+      }
+    })
+
+    vx -= 4;
+    if (vx < -1920) vx = 0;
+  }, fps);
+};
+
+// Начало создания вражеских юнитов
+function addEnemy() {
+  setTimeout(() => createEnemy(), 3000);
+}
+
+// Создание вражеских юнитов
+function createEnemy() {
+  spanwTimer = setInterval(() => {
+    let y = Math.round(Math.random() * (canvas.height - 120) + 60);
+    let enemy = new EnemyPlane(canvas.width, y);
+    enemies.push(enemy);
+  }, spawnTime);
+}
+
+// Изменение счета
+function incScore(value) {
+  scoreCount += value;
+  document.querySelector('#score-counter').innerHTML = scoreCount;
+}
+
+// Изменение количества жизней
+function decLives(value) {
+  livesCount -= value;
+  document.querySelector('#lives-counter').innerHTML = livesCount;
+}
+
+const clouds_1 = new Image(); clouds_1.src = './assets/game_background_1/layers/clouds_1.png';
+const clouds_2 = new Image(); clouds_2.src = './assets/game_background_1/layers/clouds_2.png';
+const clouds_3 = new Image(); clouds_3.src = './assets/game_background_1/layers/clouds_3.png';
+const clouds_4 = new Image(); clouds_4.src = './assets/game_background_1/layers/clouds_4.png';
+
+const player_img = new Image(); player_img.src = './assets/Plane/Fly(1).png';
+const enemy_img = new Image(); enemy_img.src = './assets/Plane/Flying_Enemy(1).png';
+
+const gun_img = new Image(); gun_img.src = './assets/Bullet/Bullet(1).png';
+
+// Начинаем игру после загрузки изображений
+if ( document.images ) {
+  renderGame();
+  addEnemy();
+}
+
+const player = new Player(10, center, player_img);
+
+canvas.addEventListener('mousemove', e => {
+  let bounds  = canvas.getBoundingClientRect();
+  let mouseY = e.clientY - bounds.top - scrollY;
+  if (canvas.height - mouseY <  60) 
+    mouseY = canvas.height - 60;
+  player.move(mouseY);
+})
+
+canvas.addEventListener('mousedown', e => {
+  let bounds  = canvas.getBoundingClientRect();
+  let mouseY = e.clientY - bounds.top - scrollY + 20;
+  if (canvas.height - mouseY <  60) 
+    mouseY = canvas.height - 40;
+  player.shoot(mouseY);
+})
+
+scoreCount.onchange
